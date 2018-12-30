8ad8adfd23b2c9db7d01a735c4dc8b35e1df03ee
richb999git <richb999@tiscali.co.uk>
2018-12-30 15:31:30 +0000

    added remote database connection code to dbh.inc.php

--- includes/dbh.inc.php
+++ includes/dbh.inc.php
@@ -1,9 +1,20 @@
 <?php
 
-$servername = "localhost";
-$dBUsername = "root";
-$dBPassword = "";
-$dBName = "loginsystem";
+
+//Get Heroku ClearDB connection information
+if (!empty(getenv("CLEARDB_DATABASE_URL"))) { // or "" or NULL
+    $cleardb_url  = parse_url(getenv("CLEARDB_DATABASE_URL"));
+    $servername   = $cleardb_url["host"];
+    $dBUsername   = $cleardb_url["user"];
+    $dBPassword   = $cleardb_url["pass"];
+    $dBName       = substr($cleardb_url["path"],1);
+}
+else {  
+    $servername = "localhost";
+    $dBUsername = "root";
+    $dBPassword = "";
+    $dBName = "loginsystem";
+}
 
 $conn = mysqli_connect($servername, $dBUsername, $dBPassword, $dBName);
 
