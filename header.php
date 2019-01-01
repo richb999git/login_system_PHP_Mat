d3453ffe99ed0a5864922a3c74a1c45f0b02a2d7
richb999git <richb999@tiscali.co.uk>
2019-01-01 14:08:45 +0000

    adj materialize init file name

--- header.php
+++ header.php
@@ -17,7 +17,7 @@
         <!-- Compiled and minified JavaScript -->
         <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
         <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-        <script src="sidenav_init.js"></script>
+        <script src="materialize_init.js"></script>
 
     </head>
     <body>  
